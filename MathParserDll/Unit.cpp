d171ce00da61bcad72cac8b66c795c4e26cd5573
Erik Bongers <mail@erikbongers.net>
2022-01-31 19:46:01 +0100

    Conversion lambdas for temperature.

--- MathParserDll/Unit.cpp
+++ MathParserDll/Unit.cpp
@@ -21,6 +21,7 @@ std::map<std::string, UnitDef> UnitDef::defs =
         { "km", UnitDef("km", 1000, UnitClass::LENGTH)},
         { "mm", UnitDef("mm", 0.001, UnitClass::LENGTH)},
         { "in", UnitDef("in", 0.0254, UnitClass::LENGTH)},
+        { "ft", UnitDef("ft", 0.3048, UnitClass::LENGTH)},
         { "thou", UnitDef("thou", 0.0254 / 1000, UnitClass::LENGTH)},
         { "yd", UnitDef("yd", 0.9144, UnitClass::LENGTH)},
         { "mi", UnitDef("mi", 1609.344, UnitClass::LENGTH)},
@@ -34,6 +35,10 @@ std::map<std::string, UnitDef> UnitDef::defs =
 
 void UnitDef::init()
     {
-    for (auto& def : defs)
-        def.second.setLambda();
+    for (auto& [id, def] : defs)
+        def.setLambda();
+    defs["C"].toSI = [](double d) { return d + 273.15; };
+    defs["C"].fromSI = [](double d) { return d - 273.15; };
+    defs["F"].toSI = [](double d) { return (d - 32) * 5 / 9 + 273.15; };
+    defs["F"].fromSI = [](double d) { return (d - 273.15) * 9 / 5 + 32; };
     }

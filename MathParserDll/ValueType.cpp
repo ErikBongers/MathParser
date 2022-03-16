aaa6e15ad5b32c8c605d717f0a319ae721111215
Erik Bongers <mail@erikbongers.net>
2022-03-17 00:37:57 +0100

    Added to_string() for ValueType

---
+++ MathParserDll/ValueType.cpp
@@ -0,0 +1,14 @@
+#include "pch.h"
+#include "ValueType.h"
+
+std::string to_string(ValueType type)
+    {
+    switch (type)
+        {
+        case ValueType::NUMBER: return "NUMBER";
+        case ValueType::TIMEPOINT: return "TIMEPOINT";
+        case ValueType::CALENDAR: return "CALENDAR";
+        case ValueType::DURATION: return "DURATION";
+        case ValueType::NONE: return "NONE";
+        }
+    }
--- MathParserDll/ValueType.h
+++ MathParserDll/ValueType.h
@@ -1,3 +1,5 @@
 #pragma once
 
 enum class ValueType : char { NONE = '_', NUMBER = 'N', TIMEPOINT = 'T', DURATION = 'D', CALENDAR = 'C' };
+
+std::string to_string(ValueType type);

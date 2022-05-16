bf03575accf6c0f487de6b5fac76043fc041f9d9
Erik Bongers <mail@erikbongers.net>
2022-05-16 21:13:45 +0200

    Refactored start-upload to REST.

--- MathParserCloudFunctions/start-script/startScript.js
+++ MathParserCloudFunctions/start-script/startScript.js
@@ -1,7 +1,6 @@
 const { Storage } = require('@google-cloud/storage');
 
-exports.uploadStartScript = async (req, res) => {
-
+exports.startScript = async (req, res) => {
 
     res.set('Access-Control-Allow-Origin', '*');
 
@@ -12,37 +11,52 @@ exports.uploadStartScript = async (req, res) => {
         res.status(204).send('');
     } else {
 
-        let sessionId = req.query.sessionId;
-
         const storage = new Storage();
 
-        let readStream = await storage
-            .bucket("mathparser-session-data")
-            .file(sessionId)
-            .createReadStream();
+        let session = await getSession(storage, req.query.sessionId);
 
-        function streamToString(stream) {
-            const chunks = [];
-            return new Promise((resolve, reject) => {
-                stream.on('data', (chunk) => chunks.push(Buffer.from(chunk)));
-                stream.on('error', (err) => reject(err));
-                stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf8')));
-            })
+        let file = storage
+            .bucket("mathparser-userdata")
+            .file("startscript-" + session.name + "-" + session.id);
+
+        if (req.method === 'GET') {
+
+            let readStream = file.createReadStream();
+            readStream.on('data', (data) => {
+                res.write(data);
+            });
+            readStream.on('end', (data) => {
+                res.status(200).send();
+            });
+
+        } else if (req.method === 'POST') {
+
+            file.save(req.body);
+            let jsonRes = {};
+            jsonRes.result = "Gelukt!" + JSON.stringify(session);
+            res.json(jsonRes);
         }
 
-        let strSession = await streamToString(readStream);
-
-        let session = JSON.parse(strSession);
-
-        await storage
-            .bucket("mathparser-userdata")
-            .file("startscript-"+session.name + "-" + session.id)
-            .save(req.body);
-
-        let jsonRes = {};
-        jsonRes.result = "Gelukt!" + JSON.stringify(session);
-        res.json(jsonRes);
-
     }
+}
 
-  };
+//await-able because returns a Promise
+function streamToString(stream) {
+    const chunks = [];
+    return new Promise((resolve, reject) => {
+        stream.on('data', (chunk) => chunks.push(Buffer.from(chunk)));
+        stream.on('error', (err) => reject(err));
+        stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf8')));
+    })
+}
+
+async function getSession(storage, sessionId) {
+    let readStream = await storage
+        .bucket("mathparser-session-data")
+        .file(sessionId)
+        .createReadStream();
+
+    let strSession = await streamToString(readStream);
+
+    return JSON.parse(strSession);
+}

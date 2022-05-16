1270aaf7835bf9a309b5c926e1ed772f8a2b6d62
Erik Bongers <mail@erikbongers.net>
2022-05-17 00:08:03 +0200

    catch error when session not found in storage.

--- MathParserCloudFunctions/get-session/getSession.js
+++ MathParserCloudFunctions/get-session/getSession.js
@@ -64,7 +64,7 @@ function streamToString(stream) {
     const chunks = [];
     return new Promise((resolve, reject) => {
         stream.on('data', (chunk) => chunks.push(Buffer.from(chunk)));
-        stream.on('error', (err) => reject(err));
+        stream.on('error', (err) => resolve(null));
         stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf8')));
     })
 }
